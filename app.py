f68df10d6bc5abeb1907df9e3bd030b1ee664e2e
alamine53 <ramzyalamine@gmail.com>
2021-01-16 12:24:58 -0500

    added server line in app

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from dash.dependencies import Input, Output
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 df = pd.read_csv('standings.csv')
 
